8d1602c56da55bc914de0734cbdbda64ba2e9c79
FattyAcid77 <117739828+FattyAcid77@users.noreply.github.com>
2026-02-12 13:54:43 +0300

    Pause Menu update v2
    added the elevator menu
    
    edited lvl_1_lobby collisions

--- characters scenes/sami.gd
+++ characters scenes/sami.gd
@@ -15,6 +15,7 @@ class_name Sami extends CharacterBody2D
 @onready var title: Label = $HUD/QuestTracker/Details/Title
 @onready var objectives: VBoxContainer = $HUD/QuestTracker/Details/Objectives
 @onready var quest_manger: Node2D = $QuestManger
+@onready var ani: AnimatedSprite2D = $Sprite2D
 
 @onready var camera =  $Camera2D
 @onready var anim_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -34,16 +35,42 @@ func _ready() -> void:
 #Keep in mind that Delta Time should be used consistently so that gameplay remains the same across different PCs, regardless of their hardware specifications
 func _physics_process(delta: float) -> void:
 	
-	if can_move:
+	if can_move and input_enabled :
+		
+		if Input.is_action_pressed("right"):
+			#velocity.x += 1
+			ani.play("Right")
+			ani.flip_h = false
+		elif Input.is_action_pressed("left"):
+			#velocity.x -= 1
+			ani.play("Left")
+			ani.flip_h = true
+		elif Input.is_action_pressed("down"):
+			#velocity.y += 1
+			ani.play("down")
+		elif Input.is_action_pressed("up"):
+			#velocity.y -= 1
+			ani.play("Up")
+		else:
+			ani.stop()
+		
+		input_direction = Input.get_vector("left", "right", "up", "down")
 		velocity = input_direction.normalized() * speed
+		#if velocity.length() > 0:
+			#velocity = velocity.normalized() * speed
+			#velocity = input_direction.normalized() * speed
+			#position += velocity * delta
+		#velocity = input_direction.normalized() * speed
+		
 		raycast()
 		move_and_slide()
 		
 
 func _process(delta: float) -> void:
-	if input_enabled:
-		input_direction = Input.get_vector("left", "right", "up", "down")
-		desired_vel = input_direction.normalized() * speed
+	#if input_enabled:
+		#input_direction = Input.get_vector("left", "right", "up", "down")
+		#desired_vel = input_direction.normalized() * speed
+		pass
 
 
 	
